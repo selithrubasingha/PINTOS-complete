c91e02b1a08915b014c30e9ad0489efa0fb2fc99
Selith <selithrubasingha@gmail.com>
2026-08-10 23:30:41 +0530

    feat: update lock struct

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,10 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem elem;      /* List element for locks held by a thread. */
+    int max_priority;          /* Maximum priority of threads waiting for the lock. */
+  
+  
   };
 
 void lock_init (struct lock *);
